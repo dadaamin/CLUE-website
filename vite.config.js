b6b107cc8ef21a35e00eeb6a4976acb9a16e41c6
Amin Dada <amin.dada@rub.de>
2024-05-05 00:58:46 +0200

    Version 0.1

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        base: '/CLUE-website/',
         plugins: [vue()],
         resolve: {
             alias: {
